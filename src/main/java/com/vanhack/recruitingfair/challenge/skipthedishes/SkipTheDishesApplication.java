eab079000861e41641cc2b93d66f4e2c954710c9
Hivison <hivison.moura@matera.com>
2018-06-23 10:23:30 -0300

    Fisrt commit

---
+++ src/main/java/com/vanhack/recruitingfair/challenge/skipthedishes/SkipTheDishesApplication.java
@@ -0,0 +1,12 @@
+package com.vanhack.recruitingfair.challenge.skipthedishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipTheDishesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkipTheDishesApplication.class, args);
+    }
+}
